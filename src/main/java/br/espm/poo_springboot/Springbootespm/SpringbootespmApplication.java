c3ecd594f3c3044a1a8f25c6280b7e5e02a1b2a8
VictorLG21 <victorgaspar1@hotmail.com>
2021-05-27 10:58:57 -0300

    criação springboot

---
+++ src/main/java/br/espm/poo_springboot/Springbootespm/SpringbootespmApplication.java
@@ -0,0 +1,13 @@
+package br.espm.poo_springboot.Springbootespm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootespmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootespmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/espm/poo_springboot/Springbootespm/SpringbootespmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.espm.poo_springboot.Springbootespm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootespmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
