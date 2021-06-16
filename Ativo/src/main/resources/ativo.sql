a1442f3e05c03e70915d7e61329fb28e57ecb7b8
VictorLG21 <victorgaspar1@hotmail.com>
2021-06-16 00:04:25 -0300

    populando banco

--- Ativo/src/main/resources/ativo.sql
+++ Ativo/src/main/resources/ativo.sql
@@ -3,3 +3,10 @@ CREATE TABLE `espm.poo.ativo`.`ativo` (
   `txNome` VARCHAR(64) NOT NULL,
   `txSimbolo` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idAtivo`));
+
+  INSERT INTO ativo
+(idAtivo,
+txNome,
+txSimbolo)
+VALUES
+("8cefc390-ce46-11eb-b8bc-0242ac130003","Petrobras",PETR4.SA);
